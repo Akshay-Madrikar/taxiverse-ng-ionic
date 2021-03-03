f84522b2b8947391d189105912a8098a27769f6c
Akshay-Madrikar <akshaymadrikar@gmail.com>
2021-03-03 22:08:55 +0530

    made some changes and updated README

---
+++ README.md
@@ -0,0 +1,4 @@
+Frontend for taxiverse project
+
+- Link to backend: https://github.com/Akshay-Madrikar/taxiverse-api
+- It includes both admin and user panels
--- src/app/admin/update-vehicle/update-vehicle.page.ts
+++ src/app/admin/update-vehicle/update-vehicle.page.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import * as moment from 'moment';
+import { Subscription } from 'rxjs';
+import { VehiclesService } from 'src/app/vehicles/vehicles.service';
 
 @Component({
   selector: 'app-update-vehicle',
   templateUrl: './update-vehicle.page.html',
   styleUrls: ['./update-vehicle.page.scss'],
 })
-export class UpdateVehiclePage implements OnInit {
+export class UpdateVehiclePage implements OnInit, OnDestroy {
+  vehicleSub: Subscription;
+  vehicleId: string;
+  minDate = moment(new Date()).format('YYYY-MM-DD');
+  maxDate = '2022-12-31';
 
-  constructor() { }
+  constructor(
+    private vehicleService: VehiclesService,
+    private loadingCtrl: LoadingController,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('vehicleId')) {
+        return;
+      }
+      this.vehicleId = paramMap.get('vehicleId');
+    });
   }
 
+  onUpdateVehicle(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const vehicleData = {
+      vehicleName: form.value['vehicleName'],
+      imageUrl: form.value['imageUrl'],
+      fuelType: form.value['fuelType'],
+      price: +form.value['price'],
+      bookable: +form.value['bookable'],
+      availableFrom: new Date(form.value['available-from']),
+      availableTo: new Date(form.value['available-to']),
+    };
+
+    this.loadingCtrl
+      .create({ message: 'Updating vehicle...' })
+      .then((loadingEl) => {
+        loadingEl.present();
+        this.vehicleSub = this.vehicleService
+          .updateVehicle(vehicleData, this.vehicleId)
+          .subscribe(() => {
+            loadingEl.dismiss();
+          });
+      });
+
+    form.reset();
+  }
+
+  ngOnDestroy() {
+    if (this.vehicleSub) {
+      this.vehicleSub.unsubscribe();
+    }
+  }
 }
